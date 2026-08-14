4024c821bf31de6e8c708151907755924dd1b069
shiven friedeman <shivenaf2005@gmail.com>
2026-08-14 10:17:05 -0700

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.java
@@ -0,0 +1,21 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        // go through each element, turn into char arr and sort
+        // arrays with matching elements are anagrams
+
+        Map<String, List<String>> hm = new HashMap<>();
+        for(int i = 0; i < strs.length; i ++){
+            char[] sortA = strs[i].toCharArray();
+            Arrays.sort(sortA);
+            String sorted = new String(sortA);
+            if(!hm.containsKey(sorted)){
+                hm.put(sorted, new ArrayList<String>());
+            }
+            hm.get(sorted).add(strs[i]);
+        }
+
+
+        return new ArrayList<>(hm.values());
+
+    }
+}
