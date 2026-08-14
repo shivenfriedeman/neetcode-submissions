775480ca86c1fc615c3b258bf68b1240cb8cebfe
shiven friedeman <shivenaf2005@gmail.com>
2026-08-13 23:15:17 -0700

    Add: validate-parentheses - submission-8

---
+++ Data Structures & Algorithms/validate-parentheses/submission-8.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isValid(String s) {
+        int slen = s.length();
+        Stack<Character> stck = new Stack<>();
+
+        if(slen % 2 == 1) return false;
+
+        for(int i = 0; i < slen; i ++){
+            if(s.charAt(i) == '(' || s.charAt(i) == '[' || s.charAt(i) == '{'){
+                stck.push(s.charAt(i));
+            }
+            if(stck.isEmpty()) return false;
+            if(s.charAt(i) == ')' && stck.pop() != '(') return false; 
+            if(s.charAt(i) == ']' && stck.pop() != '[') return false;
+            if(s.charAt(i) == '}' && stck.pop() != '{') return false;
+            
+        }
+        return stck.isEmpty();
+        
+    }
+}
